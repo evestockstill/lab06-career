0d35afbb9e8fa69efea1bb9901987984f5e70751
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-09 13:15:50 -0800

    setup dev

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const app = require('./lib/app');
+const colors = require('colors')
+const { createServer } = require('net')
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(7890, () => {
-  console.log('Application started!');
+  console.log('Application started!'.bgCyan);
 });
