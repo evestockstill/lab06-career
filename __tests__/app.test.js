25c273eb545e1d12180e79b4c75bd4ca2ac4a6c0
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-09 15:15:44 -0800

    working get route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+it('it returns plain text hi', () => {
+  return request(app)
+  .get('/')
+  .then(res => {
+    expect(res.text).toEqual('hi')
+  })
+});
 
+it('it has html <h1>red</h1>', () => {
+  return request(app)
+  .get('./red')
+  .then(res => {
+    expect(res.html).toBe('<h1>red</h1>')
+  })
+})
 });
